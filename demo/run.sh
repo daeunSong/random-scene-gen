44c25eeb05781ca586164f6d8f7c51ffb2f69ad9
daeunsong <daeunsong>
2020-04-17 00:01:27 +0900

    add demo files

--- demo/run.sh
+++ demo/run.sh
@@ -2,7 +2,7 @@
 
 gepetto-gui &
 hpp-rbprm-server &
-ipython -i --no-confirm-exit ./$1 ./$2
+ipython -i --no-confirm-exit ./lp_urdfs_path.py ./$1
 
 pkill -f  'gepetto-gui'
 pkill -f  'hpp-rbprm-server'
